a6d44dd115c589485ee22bad7dfe4d19a27d2d3a
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-22 09:27:15 +0200

    added updater function and online users function

--- client/script.js
+++ client/script.js
@@ -4,23 +4,67 @@ let socket = io.connect();
 const theInput = document.getElementById('sendForm');
 const display = document.getElementById('display');
 const input = document.getElementById('message');
+// popup updater of people joining an leaving ELEMENT
+const onlineUsersDisplay = document.getElementById('online-users');
+// where to show the online users
+const onlineDisplay = document.getElementById('onlineDisplay');
 
 let userName = prompt('choose your user name');
+socket.emit('new-user', userName);
 
+//sending message
 theInput.addEventListener('submit', e => {
     e.preventDefault()
     const message = input.value;
     socket.emit('sendMessage', userName +': ' + message);
     input.value = ' ';
-})
+});
+
 
 socket.on('sendMessage', data => {
     displayMessage(data);
 })
+socket.on('user-connected', data => {
+    usersUpdater(data);
+})
+
+socket.on('showUsers', data => {
+    onlineDisplay.append(data[socket.id])
+})
+
+socket.on('userDisconnected', data => {
+    usersUpdater(data);
+
+})
+
+
+socket.on('onlinePeople', data => {
+    showOnlineUsers(data);
+})
 
 function displayMessage(message){
-    const popup = messageElement = document.createElement('h4');
-    popup.innerText = message;
+    const popup = document.createElement('h4');
+    popup.innerText = `${message}`;
     display.append(popup);
+}
+function usersUpdater(message){
+    const popup = document.createElement('p');
+    popup.innerText = `${message} left the chat`;
+    onlineUsersDisplay.append(popup);
+    setTimeout(function(){
+        onlineUsersDisplay.removeChild(popup);
+    }, 4000);
+}
+
+function showOnlineUsers(array){
+    onlineUsersDisplay.innerHTML = "";
+
+    array.forEach(user =>{
+        console.log(user)
+        let list = document.createElement('li');
+        list.innerText = user;
+        onlineUsersDisplay.append(list);
+    })
 
 }
+
